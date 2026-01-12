4af71a73fa8f3c11c721b068e552ce187b0c627a
Wino <baulajohndarwin@gmail.com>
2026-01-12 23:09:14 +0800

    Fixed a bun in time where single digit minutes doesnt display in 00 format

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,13 +1,17 @@
 setInterval(() => {
   let time = new Date();
   let hour = time.getHours();
-  let minute = time.getMinutes();
+  let minute = (time.getMinutes()).toString();
   let meridiem;
 
   if (hour > 12) {
     hour -= 12;
     meridiem = "PM";
   } else meridiem = "AM"
+
+  if (!minute[1]) {
+    minute = '0' + minute;
+  }
 
   document.querySelector('.js-clock').innerText = `${hour}:${minute} ${meridiem}`;
   
