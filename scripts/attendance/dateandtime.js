5c76e67944019d5c3eb2d946f1b529fd973335c9
Wino <baulajohndarwin@gmail.com>
2026-01-13 17:42:46 +0800

    Moved all code in dateandtime.js into functions

--- scripts/attendance/dateandtime.js
+++ scripts/attendance/dateandtime.js
@@ -1,30 +1,45 @@
-setInterval(() => {
-  let time = new Date();
-  let hour = time.getHours();
-  let minute = (time.getMinutes()).toString();
-  let meridiem;
-
-  if (hour >= 12) {
-    hour -= 12;
-    meridiem = "PM";
-  } else meridiem = "AM"
-
-  if (hour == 0) {
-    hour = 12;
-  }
-
-  if (!minute[1]) {
-    minute = '0' + minute;
-  }
-
-  document.querySelector('.js-clock').innerText = `${hour}:${minute} ${meridiem}`;
-  
-},1000);
-
 const time = new Date();
-const month = time.toLocaleString('default', {month: 'long'});
-const day = time.getDate();
-const year = time.getFullYear();
-const fullDate = `${month} ${day}, ${year}`;
 
-document.getElementById('date').innerText = fullDate;
+export function renderTime() {
+  setInterval(() => {
+    let hour = time.getHours();
+    let minute = (time.getMinutes()).toString();
+    let meridiem;
+
+    if (hour >= 12) {
+      hour -= 12;
+      meridiem = "PM";
+    } else meridiem = "AM"
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (!minute[1]) {
+      minute = '0' + minute;
+    }
+
+    document.querySelector('.js-clock').innerText = `${hour}:${minute} ${meridiem}`;
+    
+  },1000);
+}
+
+
+export function renderDate() {
+  const month = time.toLocaleString('default', {month: 'long'});
+  const day = time.getDate();
+  const year = time.getFullYear();
+  const fullDate = `${month} ${day}, ${year}`;
+
+  document.getElementById('date').innerText = fullDate;
+}
+
+
+
+export function initCurrentDate() {
+  const month = time.getMonth() + 1;
+  const day = time.getDate();
+  const year = time.getFullYear();
+
+  return `${month}-${day}-${year}`;
+}
