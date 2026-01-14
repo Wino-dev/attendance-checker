bb3fb002ca42283c7fe9f16a5de85bab929fd718
Wino <baulajohndarwin@gmail.com>
2026-01-15 01:23:24 +0800

    adjusted the format of the output xlsx

--- scripts/attendance/export.js
+++ scripts/attendance/export.js
@@ -5,7 +5,9 @@ export function initExportXLSX(subjectName, date) {
   document.querySelector('.js-export-button').addEventListener('click', () => {
     const workbook = XLSX.utils.book_new();
     const worksheet = XLSX.utils.json_to_sheet(students);
+    worksheet["!cols"] = [ { wch: 35 }, { wch: 15 } ]; 
     XLSX.utils.book_append_sheet(workbook, worksheet, date);
+    XLSX.utils.sheet_add_aoa(worksheet, [["Name", "Student ID", "Status"]], { origin: "A1" });
     XLSX.writeFile(workbook, `${subjectName + '-' + date}.xlsx`);
   })
 }
