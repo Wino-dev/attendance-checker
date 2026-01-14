fc42997044eed14cd25848c7b4a845c0e43789af
Wino <baulajohndarwin@gmail.com>
2026-01-15 01:28:57 +0800

    Removed the direct imports of each script in main.js

--- scripts/attendance/main.js
+++ scripts/attendance/main.js
@@ -1,17 +1,19 @@
-import {renderTime, renderDate, initCurrentDate} from './dateandtime.js';
-import './input.js';
-import './subjectFunc.js';
+import { renderTime, renderDate, initCurrentDate, renderSubjectDetails} from './dateandtime.js';
 import { initSelectedSubject } from '../../data/subjects.js';
 import { renderStats } from './stats.js';
 import { students } from '../../data/students.js';
 import { renderTable } from './table.js';
 import { initExportXLSX } from './export.js';
+import { initLogButton } from './input.js';
 
+const selectedSubject = initSelectedSubject();
+console.log(selectedSubject);
+console.log(students);
 renderStats(students);
+initLogButton(selectedSubject);
 renderTable();
 renderTime();
 renderDate();
 
-const selectedSubject = initSelectedSubject();
-
-initExportXLSX(selectedSubject.name, initCurrentDate());
+renderSubjectDetails(selectedSubject);
+initExportXLSX(selectedSubject.subjectCode, initCurrentDate());
