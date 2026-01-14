68b38290e6653c977eb906d7e23c4f6bad34b780
Wino <baulajohndarwin@gmail.com>
2026-01-15 01:33:07 +0800

    Made the the subject button container and the import buttons interactive

--- scripts/index/main.js
+++ scripts/index/main.js
@@ -1,3 +1,7 @@
+import { students } from "../../data/students.js";
+import { subjects } from "../../data/subjects.js";
 import { initImportButtons } from "./importMain.js";
+import { updateButtonContainer } from "./subjectButtons.js";
 
+updateButtonContainer(students, subjects);
 initImportButtons();
