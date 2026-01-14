58eb0d14dea73da2e34927a25276db2d28d3bf64
Wino <baulajohndarwin@gmail.com>
2026-01-15 01:25:27 +0800

    created a function that initializes the log button interactivity

--- scripts/attendance/input.js
+++ scripts/attendance/input.js
@@ -1,34 +1,36 @@
 import { students } from "../../data/students.js";
-import { subjects } from "../../data/subjects.js";
 import { renderStats } from "./stats.js";
 import { renderTable } from "./table.js";
 import { updateStudentStatus } from "./studentFunc.js";
 import { getStartTimeDate } from "./subjectFunc.js";
 import { updateMessageBox } from "./message.js";
 
-document.querySelector('.js-log-button').addEventListener('click', () => {
+export function initLogButton(subject) {
+  document.querySelector('.js-log-button').addEventListener('click', () => {
   
-  const idInput = document.querySelector('.js-id-input').value;
+    const idInput = document.querySelector('.js-id-input').value;
 
-  if (!idInput) {
-    return;
-  }
+    if (!idInput) {
+      return;
+    }
 
-  let matchingStudent;
+    let matchingStudent;
 
-  students.forEach((student) => {
-    if (idInput == student.studentId) {
-      matchingStudent = student;
+    students.forEach((student) => {
+      if (idInput == student.studentId) {
+        matchingStudent = student;
+      }
+    });
+
+    if (matchingStudent) {
+      const startTime = getStartTimeDate(subject.startTime);
+      updateStudentStatus(matchingStudent, startTime);
+      renderStats(students);
+      renderTable();
+      updateMessageBox(matchingStudent.studentName, matchingStudent.status);
+    } else {
+      alert("Student doesn't exist")
     }
   });
+}
 
-  if (matchingStudent) {
-    const startTime = getStartTimeDate(subjects[0].startTime);
-    updateStudentStatus(matchingStudent, startTime);
-    renderStats(students);
-    renderTable();
-    updateMessageBox(matchingStudent.name, matchingStudent.status);
-  } else {
-    alert("Student doesn't exist")
-  }
-});
