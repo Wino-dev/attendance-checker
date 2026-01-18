edcebe9494d815beed5899517e80f083ecf42e53
Wino <baulajohndarwin@gmail.com>
2026-01-18 20:32:41 +0800

    Initialized the custom.html and the custom button

--- scripts/index/customButtons.js
+++ scripts/index/customButtons.js
@@ -2,4 +2,8 @@ export function initCustomButtons () {
   document.getElementById('redirect-to-edit').addEventListener('click', () => {
     window.location.href = './edit.html';
   });
+
+  document.getElementById('redirect-to-custom').addEventListener('click', () => {
+    window.location.href = './custom.html';
+  });
 }
